bfce8e2b2fda7d1f04f404e76fa5806aa93d23e2
ZanderalleX <alexanderjosuegradoscastro@gmail.com>
2026-05-21 12:48:11 -0500

    Se agregaron módulos de plantas y roles

---
+++ ECOLIM_ADMIN/src/modelo/Rol.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.time.LocalDateTime;
+
+public class Rol {
+
+    private long id;
+    private LocalDateTime createdAt;
+    private String name;
+
+    public Rol() {
+    }
+
+    public Rol(long id, LocalDateTime createdAt, String name) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
